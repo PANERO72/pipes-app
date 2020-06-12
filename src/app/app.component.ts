6ed2aeb10778dff8df7db4f951e59349bfcfb5c2
José Panero <panerootero@gmail.com>
2020-06-13 01:39:03 +0200

    Aplicación demo de los Pipes de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { resolve } from 'dns';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes-app';
+
+  nombre: string = "Panero";
+
+  matriz = [1,2,3,4,5,6,7,8,9,10];
+
+  PI: number = Math.PI;
+
+  porcentaje: number = 0.234; 
+
+  salario: number = 1250.50;
+
+  fecha: Date = new Date();
+
+  heroes = {
+    nombre: 'José', apellido: 'Panero', edad: '47', direccion: { calle: 'Bruc', numero: '19', piso: '2º'}
+  }
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('Se obtuvieron los datos');
+    }, 4500);
+  });
 }
